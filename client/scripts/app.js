6917da5c7c171063904848ff5fd0d042a9d30a79
himynameisjeremy <himynameisjeremy@gmail.com>
2016-04-22 14:00:50 -0500

    added to admincontroller.js and enabled routes in app.js, added emailList.html view and setVariables.html view.  Currently true index is named index2.html - note for later.  Admin.html is currently index.html in this view in order to test on localhost.  Will chance that before next commit.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,16 +1,24 @@
 var chickApp = angular.module("chickApp", ['ngMaterial', 'ngMessages', 'ngRoute']);
 
 chickApp.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider){
-    // $routeProvider.
+    $routeProvider.
     //     when("/", {
     //         templateUrl: "/views/index.html",
     //         controller: "InputController"
-    //     }).
-    //     when("/admin", {
-    //         templateUrl: "/views/admin.html",
-    //         controller: "AdminController"
-    //     }).
-    //     otherwise({
-    //         redirectTo: '/'
-    //     });
+        // }).
+        when("/admin", {
+            templateUrl: "/views/index.html",
+            controller: "AdminController"
+        }).
+        when("/emailList", {
+            templateUrl: "/views/partials/emailList.html",
+            controller: "EmailController"
+        }).
+        when("/setVariables", {
+            templateUrl: "/views/partials/setVariables.html",
+            controller: "SetVariablesController"
+        }).
+        otherwise({
+            redirectTo: '/emailList'
+        });
 }]);
--- client/scripts/controllers/admincontrol.js
+++ client/scripts/controllers/admincontrol.js
@@ -1,4 +1,9 @@
 chickApp.controller('AdminController',  ['$scope', '$http', '$window', 'ClientService','AdminService',function($scope, $http, $window, ClientService, AdminService) {
-
+  $scope.toEmailList = function(){
+    $window.location.href = '#emailList';
+  };
+  $scope.toDefaultValues = function(){
+    $window.location.href = '#setVariables';
+  };
 
 }]);
