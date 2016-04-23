3ce79c630aa231d218d4010e8daf305af9cc5fdf
Miles Johnson <milesjohnsonwashere@gmail.com>
2016-04-23 12:16:52 -0500

    updated with current repo master, about to create route for changing calculator variables, change factory with http call(s) to save inputs for calc vars to DB

--- server/models/default.js
+++ server/models/default.js
@@ -19,3 +19,49 @@ var Default_Values = new Schema({
 });
 
 module.exports = mongoose.model("Default_Values", Default_Values);
+
+
+/* TODO
+
+max, min, and default needed:
+
+target home price
+
+down payment percentange
+
+annual income
+
+annual homeowners insurance percentage
+
+association fees
+
+maintenence percentage
+
+montly additional expenses
+
+annual home value increase
+
+annual rental increase
+
+monthly rent if you rented instread of buying
+*/
+
+
+/* TODO setup routes for these values then setup http calls in fasctory
+$scope.monthlyRentPersonal=1500; //max min default
+$scope.monthlyRentTenant=1600; //max min default
+$scope.targetPrice=300000;  //max min default
+$scope.downPaymentPercentage=.5;  //max min default
+$scope.mortgageRate=1000;  //max min default
+$scope.yearsAmmoritized=500; //max min default
+$scope.income=100;  //max min default
+$scope.mortgageYears=30;  //max min default
+$scope.vacancy=.05;  //max min default
+$scope.propertyTaxPercentage=.0165  //max min default
+$scope.assocDues=0;  //max min default
+$scope.management=0; //max min default
+$scope.misc=1000;  //max min default
+$scope.insuranceRate=.01;  //max min default
+$scope.utilsRate=.009;  //max min default
+$scope.legalAccounting=100;  //max min default
+*/
