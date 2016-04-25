d348e2991d2b3136b9cf47177e78783e37b222db
Miles Johnson <milesjohnsonwashere@gmail.com>
2016-04-25 15:18:33 -0500

    about to pull down from master repo with enrique`s changes

--- server/models/default.js
+++ server/models/default.js
@@ -1,21 +1,59 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+// TODO enter min and max values.
+
 var Default_Values = new Schema({
-  zip_code: { type: Number},
-  monthly_rent: { type: Number },
-  buying_budget: { type: Number },
-  estimated_rental_income: { type: Number },
-  amortization_interest_rate: { type: Number },
-  downpayment: { type: Number },
-  property_taxes: {type: Number },
+
+  monthly_rent_def: { type: Number },
+  monthly_rent_min: { type: Number },
+  monthly_rent_max: { type: Number },
+
+  buying_budget_def: { type: Number },
+  buying_budget_min: { type: Number },
+  buying_budget_max: { type: Number },
+
+  estimated_rental_income_def: { type: Number },
+  estimated_rental_income_min: { type: Number },
+  estimated_rental_income_max: { type: Number },
+
+  amortization_interest_rate_def: { type: Number },
+  amortization_interest_rate_min: { type: Number },
+  amortization_interest_rate_max: { type: Number },
+
+  downpayment_def: { type: Number },
+  downpayment_min: { type: Number },
+  downpayment_max: { type: Number },
+
+  property_taxes_def: {type: Number },
+  property_taxes_min: { type: Number },
+  property_taxes_max: { type: Number },
+
   toggle_married_or_single: { type: Boolean },
-  annual_income: {type: Number },
-  maintainence_costs: {type: Number },
-  additional_monthly_expenses: { type: Number },
-  home_value_increase: { type: Number },
-  rental_increase: { type: Number },
-  auto_calc_depreciation: { type: Number }
+
+  annual_income_def: {type: Number },
+  annual_income_min: { type: Number },
+  annual_income_max: { type: Number },
+
+  maintainence_costs_def: {type: Number },
+  maintainence_costs_min: { type: Number },
+  maintainence_costs_max: { type: Number },
+
+  additional_monthly_expenses_def: { type: Number },
+  additional_monthly_expenses_min: { type: Number },
+  additional_monthly_expenses_max: { type: Number },
+
+  home_value_increase_def: { type: Number },
+  home_value_increase_min: { type: Number },
+  home_value_increase_max: { type: Number },
+
+  rental_increase_def: { type: Number },
+  rental_increase_min: { type: Number },
+  rental_increase_max: { type: Number },
+
+  auto_calc_depreciation_def: { type: Number },
+  auto_calc_depreciation_min: { type: Number },
+  auto_calc_depreciation_max: { type: Number }
 });
 
 module.exports = mongoose.model("Default_Values", Default_Values);
--- server/routes/default.js
+++ server/routes/default.js
@@ -9,22 +9,50 @@ router.route('/defaults')
             res.send(defaults);
         });
     })
+    /*  the following post route will only ever be used once.
+     *  it is there to provide the initial values for the calculator
+     *  which will be generated everytime this application is
+     *  loaded fresh on a new server.
+     */
     .post(function(req, res){
         var defaults = new Default({
-            zip_code: req.body.  ,
-            monthly_rent: req.body.  ,
-            buying_budget: req.body.  ,
-            estimated_rental_income: req.body.  ,
-            amortization_interest_rate: req.body.  ,
-            downpayment: req.body.  ,
-            property_taxes: req.body.  ,
+            monthly_rent_def: req.body.  ,
+            monthly_rent_min: req.body.  ,
+            monthly_rent_max: req.body.  ,
+            buying_budget_def: req.body.  ,
+            buying_budget_min: req.body.  ,
+            buying_budget_max: req.body.  ,
+            estimated_rental_income_def: req.body.  ,
+            estimated_rental_income_min: req.body.  ,
+            estimated_rental_income_max: req.body.  ,
+            amortization_interest_rate_def: req.body.  ,
+            amortization_interest_rate_min: req.body.  ,
+            amortization_interest_rate_max: req.body.  ,
+            downpayment_def: req.body.  ,
+            downpayment_min: req.body.  ,
+            downpayment_max: req.body.  ,
+            property_taxes_def: req.body.  ,
+            property_taxes_min: req.body.  ,
+            property_taxes_max: req.body.  ,
             toggle_married_or_single: req.body.  ,
-            annual_income: req.body.  ,
-            maintainence_costs: req.body.  ,
-            additional_monthly_expenses: req.body.  ,
-            home_value_increase: req.body.  ,
-            rental_increase: req.body.  ,
-            auto_calc_depreciation: req.body.
+            annual_income_def: req.body.  ,
+            annual_income_min: req.body.  ,
+            annual_income_max: req.body.  ,
+            maintainence_costs_def: req.body.  ,
+            maintainence_costs_min: req.body.  ,
+            maintainence_costs_max: req.body.  ,
+            additional_monthly_expenses_def: req.body.  ,
+            additional_monthly_expenses_min: req.body.  ,
+            additional_monthly_expenses_max: req.body.  ,
+            home_value_increase_def: req.body.  ,
+            home_value_increase_min: req.body.  ,
+            home_value_increase_max: req.body.  ,
+            rental_increase_def: req.body.  ,
+            rental_increase_min: req.body.  ,
+            rental_increase_max: req.body.  ,
+            auto_calc_depreciation_def: req.body.  ,
+            auto_calc_depreciation_min: req.body.  ,
+            auto_calc_depreciation_max: req.body.
         });
     })
 
@@ -37,7 +65,7 @@ module.exports = router;
 1. create entire list of vars from front end
 2. modify schema to match
 3. create code tp save to DB
-4. create .put request
+4. create .put request on line 53
 5. figure out how the hell we'll save intial vars once with .post request then
     only ever use .put afterwards to update.
 
