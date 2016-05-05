5459e2cc0210f77eba371d66fadc1ecd099fd71b
Enrique Ortega <enriqueortegatc@gmail.com>
2016-05-05 13:19:49 -0500

    fix register route, yolo

--- client/scripts/factories/registrationfactory.js
+++ client/scripts/factories/registrationfactory.js
@@ -6,7 +6,7 @@ chickAppRegistration.factory('RegisterFactory', ['$http', function($http){
     var registerObject = {};
     registerObject.username = username;
     registerObject.password = password;
-    $http.post('/admin', registerObject).success(function(response){
+    $http.post('/reg', registerObject).success(function(response){
       console.log("We Dun Did It");
     });
   }
